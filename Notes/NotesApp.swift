b25adc301aaf4901baff002d28fcc39d35cf1182
mttAlexandre <alexandre.montcuit@icloud.com>
2022-08-20 21:45:08 +0200

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Alexandre MONTCUIT on 20/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
